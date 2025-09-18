326aa508ea7ede4b6231645e98cc7f8c51d323f2
Walter Ngo <ngo.walter@gmail.com>
2025-09-18 11:08:53 +1000

    [CAD-715] Add support for in-line Next.js SSR scripts

--- src/utils/script/inline.ts
+++ src/utils/script/inline.ts
@@ -9,6 +9,8 @@ import type { PageScriptElement } from '../../types/page'
 export function tryGetIdFromInLineScriptCode(pageScriptElement: PageScriptElement): string {
   if (isCloudflareBotFightScript(pageScriptElement)) {
     return 'inline_script/cloudflare-bot-fight'
+  } else if (isNextJsServerSideRenderingScript(pageScriptElement)) {
+    return 'inline_script/nextjs-ssr'
   } else {
     return 'inline_script/id_not_found'
   }
@@ -19,5 +21,13 @@ export function tryGetIdFromInLineScriptCode(pageScriptElement: PageScriptElemen
  */
 function isCloudflareBotFightScript(pageScriptElement: PageScriptElement): boolean {
   const innerScriptSrcCode = "a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js'"
+  return pageScriptElement.content.includes(innerScriptSrcCode)
+}
+
+/*
+ Try to detect known in-line script code for Next.js [Server-side Rendering](https://github.com/vercel/next.js/discussions/42170#discussioncomment-8880248)
+ */
+function isNextJsServerSideRenderingScript(pageScriptElement: PageScriptElement): boolean {
+  const innerScriptSrcCode = 'self.__next_f.push'
   return pageScriptElement.content.includes(innerScriptSrcCode)
 }
